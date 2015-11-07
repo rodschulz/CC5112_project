1be8a9088939ef44c97f96e8cac1b8b4677e4092
rodrigo <rodrigo@gato>
2015-11-06 21:42:37 -0300

    Implemented pass of the target image using command line and save of the sample data

--- pix.py
+++ pix.py
@@ -1,21 +1,32 @@
 import sys
+import json
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
 ##################################################
 ##### Global variables
-im = None
+imgw = 800 
+imgh = 600
+img = None
 color = None
-
+data = {}
+dest = ''
 
 ##################################################
 ##### Store the data of the clicked pixel
 def getPixelData(event):
+	global data
+
 	x = event.pos().x()
 	y = event.pos().y() 
-	rgb = QColor(im.pixel(x, y)).toRgb()
+	rgb = QColor(img.pixel(x, y)).toRgb()
+	r = rgb.red()
+	g = rgb.green()
+	b = rgb.blue()
 
-	print('[' +str(x) + ',' + str(y) + '] => [' + str(rgb.red()) + ', ' + str(rgb.green()) + ', ' + str(rgb.blue()) + '] (' + color + ')')
+	print('[' +str(x) + ',' + str(y) + '] => [' + str(r) + ', ' + str(g) + ', ' + str(b) + '] (' + color + ')')
+
+	data[color].append([x, y, r, g, b])
 
 ##################################################
 ##### Register the currently selected color
@@ -26,24 +37,25 @@ def colorChanged(event):
 ##################################################
 ##### Register the currently selected color
 def saveSamples(event):
-	print('save data!')
+	print('Saving data to ' + dest)
+	with open(dest, 'w') as outfile:
+    		json.dump(data, outfile)
 
 ##################################################
 ##### Main app
 
 class Sampler(QWidget):
-	def __init__(self):
+	def __init__(self, imgLocation):
 		super(Sampler, self).__init__()
-		self.initUI()
+		self.initUI(imgLocation)
+		self.initDict()
 
-	def initUI(self):
-		global im
+	def initUI(self, imgLocation):
+		global img
 		global color
 
 		margin = 2
 		toolbar = 30
-		imgw = 800 
-		imgh = 600
 
 		button = QPushButton('Save', self)
         	button.clicked.connect(saveSamples)
@@ -63,9 +75,9 @@ class Sampler(QWidget):
 		color = str(combo.currentText())
 
 		# load the image, generate a label and put the image in it
-		pixmap = QPixmap('test.tiff')
+		pixmap = QPixmap(imgLocation)
 		pixmap = pixmap.scaled(imgw, imgh, Qt.KeepAspectRatio, transformMode = Qt.SmoothTransformation)
-		im = pixmap.toImage()
+		img = pixmap.toImage()
 		label = QLabel(self)
 		label.setPixmap(pixmap)
 
@@ -82,11 +94,26 @@ class Sampler(QWidget):
 		self.setWindowTitle('Train image')
 		self.show()
 
+	def initDict(self):
+		global data
+		data["Black"] = []
+		data["Blue"] = []
+		data["Green"] = []
+		data["Orange"] = []
+		data["Red"] = []
+		data["White"] = []
+		data["Yellow"] = []
+
 ##################################################
 ##### main method #####
 def main():
+	global dest
+	name = sys.argv[1][sys.argv[1].rfind('/') + 1:]
+	dest = './output/' + name.replace('.', '_') + '.json'
+	print(dest)
+
 	app = QApplication(sys.argv)
-	splr = Sampler()
+	splr = Sampler(sys.argv[1])
 	sys.exit(app.exec_())
 
 
