e92c2a3a645be79caa8603622c822c63e438389a
rodrigo <rodrigo@gato>
2015-11-06 21:09:13 -0300

    Transformed script to hold the combobox, image and button in one widget

--- pix.py
+++ pix.py
@@ -6,7 +6,7 @@ from PyQt4.QtCore import *
 ##### Global variables
 im = None
 color = None
-dest = None
+
 
 ##################################################
 ##### Store the data of the clicked pixel
@@ -24,40 +24,73 @@ def colorChanged(event):
 	color = str(event)
 
 ##################################################
+##### Register the currently selected color
+def saveSamples(event):
+	print('save data!')
+
+##################################################
 ##### Main app
 
-# generate app
-app = QApplication(sys.argv)
+class Sampler(QWidget):
+	def __init__(self):
+		super(Sampler, self).__init__()
+		self.initUI()
 
-# load the image, generate a label and put the image in it
-pixmap = QPixmap('test.tiff')
-pixmap = pixmap.scaled(800, 600, Qt.KeepAspectRatio, transformMode = Qt.SmoothTransformation)
-im = pixmap.toImage()
-label = QLabel()
-label.setPixmap(pixmap)
+	def initUI(self):
+		global im
+		global color
 
-# set title, callback and show the image
-label.setWindowTitle('Train image')
-label.mousePressEvent = getPixelData
-label.show()
+		margin = 2
+		toolbar = 30
+		imgw = 800 
+		imgh = 600
 
-# create a combobox to select the which will be taken samples
-combo = QComboBox()
-combo.addItem("Black")
-combo.addItem("Blue")
-combo.addItem("Green")
-combo.addItem("Orange")
-combo.addItem("Red")
-combo.addItem("White")
-combo.addItem("Yellow")
+		button = QPushButton('Save', self)
+        	button.clicked.connect(saveSamples)
 
-# set the selected index and get the selected color
-combo.setCurrentIndex(0)
-color = str(combo.currentText())
+		# create a combobox to select the which will be taken samples
+		combo = QComboBox(self)
+		combo.addItem("Black")
+		combo.addItem("Blue")
+		combo.addItem("Green")
+		combo.addItem("Orange")
+		combo.addItem("Red")
+		combo.addItem("White")
+		combo.addItem("Yellow")
 
-# set callback and show the combobox
-combo.activated[str].connect(colorChanged)  
-combo.show()
+		# set the selected index and get the selected color
+		combo.setCurrentIndex(0)
+		color = str(combo.currentText())
 
-# wait until the image dialog is closed
-sys.exit(app.exec_())
+		# load the image, generate a label and put the image in it
+		pixmap = QPixmap('test.tiff')
+		pixmap = pixmap.scaled(imgw, imgh, Qt.KeepAspectRatio, transformMode = Qt.SmoothTransformation)
+		im = pixmap.toImage()
+		label = QLabel(self)
+		label.setPixmap(pixmap)
+
+		# set callbacks and show the image
+		label.mousePressEvent = getPixelData
+		combo.activated[str].connect(colorChanged)
+
+		# set sizes and geometry and show
+		combo.move(margin, margin)
+		button.move(combo.size().width(), margin)
+		label.move(margin, toolbar + margin)
+		
+		self.setGeometry(300, 300, imgw + margin * 2, imgh + margin * 2 + toolbar)
+		self.setWindowTitle('Train image')
+		self.show()
+
+##################################################
+##### main method #####
+def main():
+	app = QApplication(sys.argv)
+	splr = Sampler()
+	sys.exit(app.exec_())
+
+
+##################################################
+##### call main method #####
+if __name__ == '__main__':
+	main()
